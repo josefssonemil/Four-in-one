1e49f6444751a8e1b9a82f87deba54c28008e9d8
Emil Josefsson <emiljos@student.chalmers.se>
2019-03-12 12:56:28 +0100

    Add initial class shells

---
+++ Kuggen/Kuggen/Controller/PairingViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PairingViewController.swift
+//  Kuggen
+//
+//  Created by Emil Josefsson on 2019-03-12.
+//  Copyright © 2019 Four-in-one. All rights reserved.
+//
+
+import Foundation
---
+++ Kuggen/Kuggen/View/GameView.swift
@@ -0,0 +1,9 @@
+//
+//  GameView.swift
+//  Kuggen
+//
+//  Created by Emil Josefsson on 2019-03-12.
+//  Copyright © 2019 Four-in-one. All rights reserved.
+//
+
+import Foundation

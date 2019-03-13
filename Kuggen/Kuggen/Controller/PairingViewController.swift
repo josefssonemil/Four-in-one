0e7ce15c3acf86c631dc782feb6d91a9c1363c0e
Emil Josefsson <emiljos@student.chalmers.se>
2019-03-13 16:52:25 +0100

    Implement parts of device pairing

--- Kuggen/Kuggen/Controller/PairingViewController.swift
+++ Kuggen/Kuggen/Controller/PairingViewController.swift
@@ -8,8 +8,36 @@ import FourInOneCore
 
 class PairingViewController : FourInOneConnectingViewController, Storyboarded {
 
+    // Coordinator which handles navigation between views
     weak var coordinator: MainCoordinator?
-
-
+    
+    
+    // Assigns delegate and starts the setup of the 4-in-1 session
+    func setupPhase(){
+        setupManager.delegate = self
+        setupManager.startSetup()
+    }
+    
+    // Makes connection by informing the setupManager that this device is ready to start the session
+    // The setupManager will let the delegate know when all devices are ready
+    func makeConnection(){
+        //Wait for all devices to be ready
+        setupManager.readyAndWaitingForPeers()
+        
+        // The gameManager which will control the game
+        var gameManager : KuggenSessionManager
+        
+        if setupManager.isServer {
+            gameManager = KuggenSessionServer()
+        }
+        else {
+            gameManager = KuggenSessionClient()
+        }
+        
+    
+        setupManager.initSessionManager(gameManager)
+        setupManager.finishSetup()
+    }
+    
 }
 

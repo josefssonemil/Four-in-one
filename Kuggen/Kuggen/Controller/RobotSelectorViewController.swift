980e77bf4267aee759beed0f0e7a494178367f94
Emil Josefsson <emiljos@student.chalmers.se>
2019-03-14 10:23:45 +0100

    Add button to navigate between selector and pairing screen

--- Kuggen/Kuggen/Controller/RobotSelectorViewController.swift
+++ Kuggen/Kuggen/Controller/RobotSelectorViewController.swift
@@ -12,5 +12,46 @@ import SpriteKit
 class RobotSelectorViewController: UIViewController, Storyboarded {
     weak var coordinator: MainCoordinator?
 
+    @IBOutlet weak var robot1Button: UIButton!
+    @IBOutlet weak var robot2Button: UIButton!
+    @IBOutlet weak var robot3Button: UIButton!
+    @IBOutlet weak var robot4Button: UIButton!
+    
+    @IBAction func robot1Selected(_ sender: Any) {
+        robot1Button.isHighlighted = true
+        robot2Button.isHighlighted = false
+        robot3Button.isHighlighted = false
+        robot4Button.isHighlighted = false
+
+    }
+    
+    @IBAction func robot2Selected(_ sender: Any) {
+        robot2Button.isHighlighted = true
+        robot1Button.isHighlighted = false
+        robot3Button.isHighlighted = false
+        robot4Button.isHighlighted = false
+
+    }
+    
+    
+    @IBAction func robot3Selected(_ sender: Any) {
+        robot3Button.isHighlighted = true
+        robot2Button.isHighlighted = false
+        robot3Button.isHighlighted = false
+        robot4Button.isHighlighted = false
+    }
+    
+    @IBAction func robot4Selected(_ sender: Any) {
+        robot4Button.isHighlighted = true
+        robot1Button.isHighlighted = false
+        robot3Button.isHighlighted = false
+        robot2Button.isHighlighted = false
+
+    }
+    
+    @IBAction func nextTapped(_ sender: Any) {
+        coordinator?.goToPairingScreen()
+        // TODO: send robot selected to next screens
+    }
     
 }

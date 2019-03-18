e8f81f8fac97401b894170d7856371d042a4a1df
Allex <allexnordgren@outlook.com>
2019-03-18 11:53:53 +0100

    initial setup fpr the robotConfigurationView

--- Kuggen/Kuggen/Controller/RobotConfigurationViewController.swift
+++ Kuggen/Kuggen/Controller/RobotConfigurationViewController.swift
@@ -9,7 +9,66 @@
 import UIKit
 import SpriteKit
 
-class RobotConfigurationViewController: UIViewController, Storyboarded {
+class RobotConfigurationViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, Storyboarded {
+    
+    @IBOutlet weak var collectionViewTop: UICollectionView!
+    @IBOutlet weak var collectionViewMid: UICollectionView!
+    @IBOutlet weak var collectionViewBot: UICollectionView!
+    
+    var imageArrayTop = [UIImage (named: "Top1"),UIImage (named: "Top2")]
+    var imageArrayMid = [UIImage (named: "Mid1"),UIImage (named: "Mid2")]
+    var imageArrayBot = [UIImage (named: "Bot1"),UIImage (named: "Bot2")]
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        if let layout = collectionViewTop.collectionViewLayout as? UICollectionViewFlowLayout {
+            let itemWidth = view.bounds.width
+            let itemHeight = collectionViewTop.bounds.height
+            layout.itemSize = CGSize(width: itemWidth, height: itemHeight)
+            layout.invalidateLayout()
+        }
+        if let layout = collectionViewMid.collectionViewLayout as? UICollectionViewFlowLayout {
+            let itemWidth = view.bounds.width
+            let itemHeight = collectionViewMid.bounds.height
+            layout.itemSize = CGSize(width: itemWidth, height: itemHeight)
+            layout.invalidateLayout()
+        }
+        if let layout = collectionViewBot.collectionViewLayout as? UICollectionViewFlowLayout {
+            let itemWidth = view.bounds.width
+            let itemHeight = collectionViewBot.bounds.height
+            layout.itemSize = CGSize(width: itemWidth, height: itemHeight)
+            layout.invalidateLayout()
+        }
+    }
+
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView == self.collectionViewTop {
+            return imageArrayTop.count // Replace with count of your data for collectionViewA
+        }else if collectionView == self.collectionViewMid {
+            return imageArrayMid.count // Replace with count of your data for collectionViewA
+        }
+        return imageArrayBot.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if collectionView == self.collectionViewTop {
+            let cell1 = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCell", for: indexPath) as! ImageCell
+            cell1.img.image = imageArrayTop[indexPath.row]
+            return cell1
+        }
+        if collectionView == self.collectionViewMid {
+            let cell2 = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCell", for: indexPath) as! ImageCell
+            cell2.img.image = imageArrayMid[indexPath.row]
+            return cell2
+            }
+            let cell3 = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCell", for: indexPath) as! ImageCell
+            cell3.img.image = imageArrayBot[indexPath.row]
+            return cell3
+    }
+    
+    
+    
     weak var coordinator: MainCoordinator?
     
     //Robot

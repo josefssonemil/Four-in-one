b8299558d2ba2e5efcf97ceec4d8e27a31ab758c
Allex <allexnordgren@outlook.com>
2019-03-18 11:53:53 +0100

    initial setup fpr the robotConfigurationView

---
+++ Kuggen/Kuggen/ImageCell.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCell.swift
+//  Kuggen
+//
+//  Created by Alexander Nordgren on 2019-03-15.
+//  Copyright © 2019 Four-in-one. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+    
+    @IBOutlet weak var img: UIImageView!
+    
+    
+}

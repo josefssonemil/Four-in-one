c3e51d6ebc8a2a890f82483270700e3aa2eaf05b
Jonatan Magnusson <jonatanmagnusson@Davids-MacBook-Pro.local>
2019-03-21 21:53:39 +0100

    implemented most of the class diagram

--- Kuggen/Kuggen/Model/Key.swift
+++ Kuggen/Kuggen/Model/Key.swift
@@ -1,2 +1,13 @@
+import SpriteKit
 
-import Foundation
+class Key {
+    var matchingLock: Lock
+    var position: CGPoint
+    
+    //Creates a key with a specified lock and position
+    init(lock: Lock, pos: CGPoint) {
+        matchingLock = lock
+        position = pos
+    }
+
+}

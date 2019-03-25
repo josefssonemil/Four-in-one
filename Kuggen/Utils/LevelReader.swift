63424b58e8e3bc2fb6b7814bf53c014fba58dc53
Tove <ektove@student.chalmers.se>
2019-03-25 12:56:24 +0100

    Started coding

---
+++ Kuggen/Utils/LevelReader.swift
@@ -0,0 +1,29 @@
+//
+//  LevelReader.swift
+//  Kuggen
+//
+//  Created by Tove Ekman on 2019-03-22.
+//  Copyright © 2019 Four-in-one. All rights reserved.
+//
+/*
+import SpriteKit
+
+
+protocol Initializable {
+    init()
+}
+
+class A : Initializable {
+    var content:String
+    
+    required init() {
+        content = "TestContent"
+    }
+}
+
+func createInstance<T where T:Initializable>(typeThing:T.Type) -> T {
+    return typeThing()
+}
+
+let a = createInstance(A)
+*/ 

a628aa125e4ed3c3e8837515f6d7578ba32dc7cc
Allex <allexnordgren@outlook.com>
2019-03-27 12:46:51 +0100

    adding a Level class to the model

---
+++ Kuggen/Kuggen/Model/Level.swift
@@ -0,0 +1,23 @@
+//
+//  Level.swift
+//  Kuggen
+//
+//  Created by Alexander Nordgren on 2019-03-27.
+//  Copyright © 2019 Four-in-one. All rights reserved.
+//
+
+import Foundation
+
+class Level{
+    var cogwheels: [Cogwheel]
+    var key: Key?
+    
+    init(cogwheels: [Cogwheel]) {
+        self.cogwheels=cogwheels
+    }
+    
+    init(cogwheels: [Cogwheel], key: Key) {
+        self.cogwheels=cogwheels
+        self.key=key
+    }
+}

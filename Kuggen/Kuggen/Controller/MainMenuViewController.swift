c4d1a7a87d596eb0e2bd9626bd34dae9a526b28c
Allex <allexnordgren@outlook.com>
2019-04-03 13:09:40 +0200

    adding a helper robot on the main menu

--- Kuggen/Kuggen/Controller/MainMenuViewController.swift
+++ Kuggen/Kuggen/Controller/MainMenuViewController.swift
@@ -12,10 +12,56 @@ class MainMenuViewController: UIViewController, Storyboarded {
     
     weak var coordinator: MainCoordinator?
     
+    private let help = ["För att starta ett spel, peka på spel" , "För att välja hur din robot ska se ut, peka på Byt robot", "För att ändra inställningar, peka på Inställningar"]
+    private var helpCount = 0
+    
     @IBOutlet weak var playButton: MenuButton!
     @IBOutlet weak var selectRobotMenuButton: MenuButton!
     @IBOutlet weak var optionMenuButton: MenuButton!
-
+    @IBOutlet weak var robotButton: UIButton!
+    @IBOutlet weak var speechBubble: UIImageView!
+    @IBOutlet weak var speechLabel: UILabel!
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.speechLabel.text = "Hej, peka på mig för att få hjälp"
+        UIView.animate(withDuration: 0.5, delay: 1, animations: {
+            self.speechLabel.alpha=1.0
+            self.speechBubble.alpha=1.0
+        }, completion: {
+            (finished) in
+            UIView.animate(withDuration: 0.5, delay: 2.0, animations: {
+                self.speechLabel.alpha=0.0
+                self.speechBubble.alpha=0.0
+            })
+        })
+    }
+    override func viewDidLoad() {
+        speechLabel.alpha = 0
+        speechBubble.alpha = 0
+        super.viewDidLoad()
+        self.navigationController?.isNavigationBarHidden = true
+        self.robotButton.setImage(UIImage(named: "robotBlink"), for: .highlighted)
+        
+    }
+    @IBAction func robotTapped(_ sender: Any) {
+        if(!(helpCount < help.count)){
+            helpCount=0
+        }
+        self.speechLabel.text = help[helpCount]
+        helpCount+=1
+        UIView.animate(withDuration: 0.5, animations: {
+            self.speechLabel.alpha=1.0
+            self.speechBubble.alpha=1.0
+        }, completion: {
+            (finished) in
+            UIView.animate(withDuration: 0.5, delay: 2.0, animations: {
+                self.speechLabel.alpha=0.0
+                self.speechBubble.alpha=0.0
+            })
+        })
+      
+    }
     
     @IBAction func playTapped(_ sender: Any) {
         LevelReader.createLevel(nameOfLevel: "level2")

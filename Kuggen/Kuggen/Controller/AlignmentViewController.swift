e625f29d7aa8ae6e0d0a9a6150b253d82ed5d4e0
Allex <allexnordgren@outlook.com>
2019-04-03 09:55:28 +0200

    adding animation for when the ipads lose cnnection or a there are three players connected

--- Kuggen/Kuggen/Controller/AlignmentViewController.swift
+++ Kuggen/Kuggen/Controller/AlignmentViewController.swift
@@ -27,6 +27,7 @@ class AlignmentViewController: FourInOneConnectingViewController, Storyboarded {
     
     @IBOutlet weak var stateLabel: UILabel!
     @IBOutlet weak var loadingCog: UIImageView!
+    @IBOutlet weak var backButton: UIButton!
     
 
     @IBAction func next(_ sender: Any) {
@@ -53,16 +54,26 @@ class AlignmentViewController: FourInOneConnectingViewController, Storyboarded {
     }
     
     override func viewDidLoad() {
-        shortBorder.isHidden=true
-        longBorder.isHidden=true
+        switch self.team {
+        case 1:
+            loadingCog.image = UIImage(named: "cogTeam1")
+        case 2:
+            loadingCog.image = UIImage(named: "cogTeam2")
+        case 3:
+            loadingCog.image = UIImage(named: "cogTeam3")
+        case 4:
+            loadingCog.image = UIImage(named: "cogTeam4")
+        default:
+            break
+        }
+        
         super.viewDidLoad()
         self.debugMode = true
-        self.navigationController?.isNavigationBarHidden = true
-        UIView.animate(withDuration: 5.0, delay: 0.0, options: [.repeat, .autoreverse], animations: {
-            self.loadingCog.transform = CGAffineTransform(rotationAngle: (.pi))
-        })
+       
         setupStartView()
     }
+    
+    
     
     @IBAction func goBack(_ sender: Any) {
         coordinator?.goToTeamSelection()
@@ -98,35 +109,113 @@ class AlignmentViewController: FourInOneConnectingViewController, Storyboarded {
         // Called when something in the setup procedure changes.
         // Update view to match the state.
         // All the visible stuff should be here
-        loadingCog.transform = CGAffineTransform.identity
+        setView(position: position, mode: mode, inProgress: inProgress)
+        shortBorder.isHidden=false
+        longBorder.isHidden=false
+
+        stateLabel.text = (inProgress ? connectingString : connectedString + position.rawValue.description)
+            }
+    
+    private func setupStartView() {
+        showDebugInfo(type: .starting)
+        teamLabel.isHidden = true
+        idLabel.isHidden = true
+        serverLabel.isHidden = true
+        peersLabel.isHidden = true
+        
+        stateLabel.text = connectingString
+        /*if !debugMode {
+            
+
+            
+        }*/
+        shortBorder.isHidden=true
+        longBorder.isHidden=true
+        self.navigationController?.isNavigationBarHidden = true
+        UIView.animate(withDuration: 2.0, animations: {
+            self.stateLabel.center.y = self.view.bounds.height/2
+            self.stateLabel.center.x = self.view.bounds.width/2 - 160
+            self.loadingCog.transform = CGAffineTransform(scaleX: 1, y: 1)
+            self.loadingCog.center.y = self.view.bounds.height/2
+            self.loadingCog.center.x = self.view.bounds.width/2 + 160
+            self.backButton.center.y = self.view.bounds.height-75
+            self.backButton.center.x = self.view.bounds.width/2
+            self.backButton.alpha=60
+        })
+        UIView.animate(withDuration: 5.0, delay: 0.0, options: [.repeat, .autoreverse], animations: {
+            self.loadingCog.transform = CGAffineTransform(rotationAngle: (.pi))
+        })
+  
+    }
+    private func setView(position : DevicePosition, mode : GameMode, inProgress: Bool) {
+        if(!inProgress){
+            loadingCog.transform = CGAffineTransform.identity
         switch position {
         case DevicePosition.one:
-            self.longBorder.frame = CGRect(x: self.view.bounds.width, y: 0, width: 0, height: 50)
-            self.shortBorder.frame = CGRect(x: self.view.bounds.width-50, y: 0, width: 50, height: 0)
-            UIView.animate(withDuration: 2.0, animations: {
-                self.loadingCog.center.y=0
-                self.loadingCog.center.x=self.view.bounds.width
-                self.loadingCog.transform = CGAffineTransform(scaleX: 3, y: 3)
-                self.longBorder.backgroundColor = self.oneTwo
-                self.shortBorder.backgroundColor = self.oneFour
-                self.longBorder.frame = CGRect(x: self.view.bounds.width, y: 0, width: -self.view.bounds.width, height: 50)
-                self.shortBorder.frame = CGRect(x: self.view.bounds.width-50, y: 0, width: 50, height: self.view.bounds.height)
-                //self.longBorder.frame = CGRect(x: self.view.bounds.width, y: 0, width: self.view.bounds.width, height: 50)
-            })
+            if(mode == GameMode.twoplayer){
+                UIView.animate(withDuration: 2.0, animations: {
+                    self.loadingCog.center.y=0
+                    self.loadingCog.center.x=self.view.bounds.width/2
+                    self.loadingCog.transform = CGAffineTransform(scaleX: 3, y: 3)
+                    self.stateLabel.center.x = self.view.bounds.width/2
+                    self.stateLabel.center.y = self.view.bounds.height/2 + 100
+                })
+            }
+            else{
+                self.longBorder.frame = CGRect(x: self.view.bounds.width, y: 0, width: 0, height: 50)
+                self.shortBorder.frame = CGRect(x: self.view.bounds.width-50, y: 0, width: 50, height: 0)
+                UIView.animate(withDuration: 2.0, animations: {
+                    self.loadingCog.center.y=0
+                    self.loadingCog.center.x=self.view.bounds.width
+                    self.loadingCog.transform = CGAffineTransform(scaleX: 3, y: 3)
+                    self.longBorder.backgroundColor = self.oneTwo
+                    self.shortBorder.backgroundColor = self.oneFour
+                    self.longBorder.frame = CGRect(x: self.view.bounds.width, y: 0, width: -self.view.bounds.width, height: 50)
+                    self.shortBorder.frame = CGRect(x: self.view.bounds.width-50, y: 0, width: 50, height: self.view.bounds.height)
+                    //self.longBorder.frame = CGRect(x: self.view.bounds.width, y: 0, width: self.view.bounds.width, height: 50)
+                })
+            }
         case DevicePosition.two:
-            self.longBorder.frame = CGRect(x: self.view.bounds.width, y: self.view.bounds.height-50, width: 0, height: 50)
-            self.shortBorder.frame = CGRect(x: self.view.bounds.width-50, y: self.view.bounds.height, width: 50, height: 0)
-            UIView.animate(withDuration: 2.0, animations: {
-                self.loadingCog.center.y=self.view.bounds.height
-                self.loadingCog.center.x=self.view.bounds.width
-                self.loadingCog.transform = CGAffineTransform(scaleX: 3, y: 3)
-                self.longBorder.backgroundColor = self.oneTwo
-                self.shortBorder.backgroundColor = self.twoThree
-                //self.longBorder.center.y = self.view.bounds.height-25
-                self.longBorder.frame = CGRect(x: self.view.bounds.width, y: self.view.bounds.height-50, width: -self.view.bounds.width, height: 50)
-                self.shortBorder.frame = CGRect(x: self.view.bounds.width-50, y: self.view.bounds.height, width: 50, height: -self.view.bounds.height)
-            })
+            stateLabel.transform = CGAffineTransform(rotationAngle: .pi)
+            if(mode == GameMode.twoplayer){
+                UIView.animate(withDuration: 2.0, animations: {
+                    self.loadingCog.center.y=self.view.bounds.height
+                    self.loadingCog.center.x=self.view.bounds.width/2
+                    self.loadingCog.transform = CGAffineTransform(scaleX: 3, y: 3)
+                    self.stateLabel.isHidden=true
+                    self.backButton.isHidden=true
+                }, completion: { (finished) in
+                    self.backButton.center.y = 75
+                    self.backButton.center.x = self.view.bounds.width/2
+                    self.stateLabel.center.x = self.view.bounds.width/2
+                    self.stateLabel.center.y = self.view.bounds.height/2 - 100
+                    self.backButton.isHidden=false
+                    self.stateLabel.isHidden=false
+                })
+            }
+            else{
+                self.longBorder.frame = CGRect(x: self.view.bounds.width, y: self.view.bounds.height-50, width: 0, height: 50)
+                self.shortBorder.frame = CGRect(x: self.view.bounds.width-50, y: self.view.bounds.height, width: 50, height: 0)
+                UIView.animate(withDuration: 2.0, animations: {
+                    self.loadingCog.center.y=self.view.bounds.height
+                    self.loadingCog.center.x=self.view.bounds.width
+                    self.loadingCog.transform = CGAffineTransform(scaleX: 3, y: 3)
+                    self.longBorder.backgroundColor = self.oneTwo
+                    self.shortBorder.backgroundColor = self.twoThree
+                    //self.longBorder.center.y = self.view.bounds.height-25
+                    self.longBorder.frame = CGRect(x: self.view.bounds.width, y: self.view.bounds.height-50, width: -self.view.bounds.width, height: 50)
+                    self.shortBorder.frame = CGRect(x: self.view.bounds.width-50, y: self.view.bounds.height, width: 50, height: -self.view.bounds.height)
+                }, completion: { (finished) in
+                    self.backButton.center.y = 75
+                    self.backButton.center.x = self.view.bounds.width/2
+                    self.stateLabel.center.x = self.view.bounds.width/2
+                    self.stateLabel.center.y = self.view.bounds.height/2 - 100
+                    self.backButton.isHidden=false
+                    self.stateLabel.isHidden=false
+                })
+            }
         case DevicePosition.three:
+            stateLabel.transform = CGAffineTransform(rotationAngle: .pi)
             self.longBorder.frame = CGRect(x: 0, y: self.view.bounds.height-50, width: 0, height: 50)
             self.shortBorder.frame = CGRect(x: 0, y: self.view.bounds.height, width: 50, height: 0)
             UIView.animate(withDuration: 2.0, animations: {
@@ -150,32 +239,13 @@ class AlignmentViewController: FourInOneConnectingViewController, Storyboarded {
                 self.longBorder.backgroundColor = self.threeFour
                 self.shortBorder.backgroundColor = self.oneFour
                 //self.shortBorder.center.x=25
-                self.longBorder.frame = CGRect(x: 0, y: 50, width: self.view.bounds.width, height: 50)
+                self.longBorder.frame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 50)
                 self.shortBorder.frame = CGRect(x: 0, y: 0, width: 50, height: self.view.bounds.height)
             })
         default:
             break
         }
-        shortBorder.isHidden=false
-        longBorder.isHidden=false
-
-        stateLabel.text = (inProgress ? connectingString : connectedString + position.rawValue.description)
-            }
-    
-    private func setupStartView() {
-        
-        showDebugInfo(type: .starting)
-        teamLabel.isHidden = true
-        idLabel.isHidden = true
-        serverLabel.isHidden = true
-        peersLabel.isHidden = true
-        
-        stateLabel.text = connectingString
-        /*if !debugMode {
-            
-
-            
-        }*/
+        }else {setupStartView() }
     }
     
     override func didStartMainActivity(_ manager: FourInOneSetupManager) {

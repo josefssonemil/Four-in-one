7145ad4a517447191e455f9800b6267b170a6c52
josefssonemil <emiljos@student.chalmers.se>
2019-04-04 10:22:56 +0200

    Remove unneeded code

--- Kuggen/Kuggen/Game/GameScene.swift
+++ Kuggen/Kuggen/Game/GameScene.swift
@@ -38,15 +38,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     private let robotFour = Robot(matchingHandle: handleFour, devicePosition: .four, textureName: "robot_4")
     private let cogWheel = Cogwheel(handle: handleOne, outer: 1.0, inner: 1.0, current: 1.0, size: CGSize.init(width: 3.0, height: 3.0), color: UIColor.blue)
     
-    //private var robotController : GameViewController!
-    //private var cogwheelController : GameViewController!
-    
-    //var entities = [GKEntity]()
-    //var graphs = [String : GKGraph]()
-    
-    //private var lastUpdateTime : TimeInterval = 0
-    //private var label : SKLabelNode?
-    //private var spinnyNode : SKShapeNode?
+
     
     // Init
     required init?(coder aDecoder: NSCoder) {
@@ -65,27 +57,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     /*override func sceneDidLoad() {
-     self.lastUpdateTime = 0
-     
-     // Get label node from scene and store it for use later
-     self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-     if let label = self.label {
-     label.alpha = 0.0
-     label.run(SKAction.fadeIn(withDuration: 2.0))
-     }
-     
-     // Create shape node to use during mouse interaction
-     let w = (self.size.width + self.size.height) * 0.05
-     self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-     
-     if let spinnyNode = self.spinnyNode {
-     spinnyNode.lineWidth = 2.5
-     
-     spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-     spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-     SKAction.fadeOut(withDuration: 0.5),
-     SKAction.removeFromParent()]))
-     }
+   
      }*/
     
     
@@ -100,9 +72,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         // set the background color
         self.backgroundColor = SKColor.white
         
-       // let test = SKSpriteNode(color: UIColor.red, size: CGSize(width: 50, height: 50))
-       // test.position = CGPoint(x: 20, y: 20)
-      //  self.addChild(test)
+   
         
         // Physics - Setup physics here
         self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
@@ -169,30 +139,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
          self.lastUpdateTime = currentTime*/
     }
     
-    
-    /*func touchDown(atPoint pos : CGPoint) {
-     if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-     n.position = pos
-     n.strokeColor = SKColor.green
-     self.addChild(n)
-     }
-     }
-     
-     func touchMoved(toPoint pos : CGPoint) {
-     if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-     n.position = pos
-     n.strokeColor = SKColor.blue
-     self.addChild(n)
-     }
-     }
-     
-     func touchUp(atPoint pos : CGPoint) {
-     if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-     n.position = pos
-     n.strokeColor = SKColor.red
-     self.addChild(n)
-     }
-     }*/
+
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
       

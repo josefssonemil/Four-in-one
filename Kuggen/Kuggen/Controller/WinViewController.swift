9d1a2073e259f74ce08997df4074cbbffbb985ea
Tove <ektove@student.chalmers.se>
2019-05-03 17:03:27 +0200

    start work on animation

--- Kuggen/Kuggen/Controller/WinViewController.swift
+++ Kuggen/Kuggen/Controller/WinViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FourInOneCore
 
 class WinViewController: UIViewController, Storyboarded {
     weak var coordinator: MainCoordinator?
@@ -20,6 +21,25 @@ class WinViewController: UIViewController, Storyboarded {
     
     @IBAction func menuTapped(_ sender: Any) {
         coordinator?.start()
+    }
+    
+    @IBAction override func viewDidAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 2.0, animations: {
+            
+       /*     self.longBorder.alpha = 0
+            self.shortBorder.alpha = 0
+            self.stateLabel.center.y = self.view.bounds.height/2
+            self.stateLabel.center.x = self.view.bounds.width/2 - 160
+            self.loadingCog.transform = CGAffineTransform(scaleX: 1, y: 1)
+            self.loadingCog.center.y = self.view.bounds.height/2
+            self.loadingCog.center.x = self.view.bounds.width/2 + 160
+            self.backButton.center.y = self.view.bounds.height-75
+            self.backButton.center.x = self.view.bounds.width/2
+ */
+        })
+        UIView.animate(withDuration: 5.0, delay: 0.0, options: [.repeat, .autoreverse], animations: {
+           // self.loadingCog.transform = CGAffineTransform(rotationAngle: (.pi))
+        })
     }
 }
 
